02218b1110cfd0541339c21a8d150d0e6ffce234
dudqls5630 <dudqls5630@gmail.com>
2020-10-21 10:54:34 +0900

    no message

--- MoonLighter-master22/player.cpp
+++ MoonLighter-master22/player.cpp
@@ -338,6 +338,7 @@ void player::playerState()
 			break;
 
 		case PLAYER_ATTACK_SWORD:
+		
 			if (_aniSword->getAniState() == ANIMATION_END)
 			{
 				_state = PLAYER_IDLE;
@@ -349,7 +350,8 @@ void player::playerState()
 			}
 			break;
 
-		case PLAYER_ATTACK_SWORD_SECOND:		
+		case PLAYER_ATTACK_SWORD_SECOND:	
+			
 			if (_aniSwordTwo->getAniState() == ANIMATION_END)
 			{
 				_state = PLAYER_IDLE;
@@ -406,7 +408,10 @@ void player::playerState()
 				}
 				_state = PLAYER_IDLE;
 				_skillCount = 0;
-				
+				if (!SOUNDMANAGER->isPlaySound("화살발사"))
+				{
+					SOUNDMANAGER->play("화살발사", 0.3f);
+				}
 			}
 			break;
 		case PLAYER_ATTACK_BOW:
@@ -671,10 +676,9 @@ void player::playerAttack()
 			case SHORT_SOWRD:
 				_state = PLAYER_ATTACK_SWORD;
 				if (!SOUNDMANAGER->isPlaySound("검휘두르기"))
-					switch (_player.weapon)
-					{
-						SOUNDMANAGER->play("검휘두르기", 0.3f);
-					}
+				{
+					SOUNDMANAGER->play("검휘두르기", 0.3f);
+				}
 				_aniSword->aniRestart();
 				break;
 
@@ -709,9 +713,13 @@ void player::playerSkill()
 			_state = PLAYER_SHILED;
 			break;
 		case BOW:
+			if (!SOUNDMANAGER->isPlaySound("화살스킬"))
+			{
+				SOUNDMANAGER->play("화살스킬", 0.3f);
+			}
 			if (!_isShoot)
 			{
-				_state = BOW_CHARGE;		
+				_state = BOW_CHARGE;
 			}
 			break;
 		}
@@ -749,6 +757,7 @@ bool player::getKeyMove()
 
 void player::arrowSkillSet()
 {
+	
 	if (_state == BOW_CHARGE)
 	{
 		_skillCount++;
